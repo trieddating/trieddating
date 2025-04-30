7ec6112dc41be8ecfe577a9b4553e35ed8d66a7a
tsologub <taras.sologub@pm.me>
2025-04-30 05:58:35 +0200

    Zoom picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -133,10 +133,6 @@ export default function Home() {
           <div className="bg-gray-100">
             <div className="container mx-auto px-4 py-16 lg:px-8 lg:py-32 xl:max-w-7xl">
               <div className="flex items-center justify-center rounded-xl border-2 border-dashed border-gray-200 bg-gray-50 py-32 text-gray-400">
-                {/*<BarChartTD />*/}
-                {/*<PieChartTD />*/}
-                {/*<BarChartMultipleTD/>*/}
-                {/*<Image src={`attractive.png`} alt={`Attractive image`} height={1406} width={2514}/>*/}
                 <Zoom src={`attractive.png`} alt={`Attractive image`} height={1406} width={2514} layout={`responsive`}/>
               </div>
             </div>
