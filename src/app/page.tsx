7ce9885e15c33543332a38a70f4d8ba6a35614f3
tsologub <taras.sologub@pm.me>
2025-05-03 05:30:44 +0200

    Fix image path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -133,7 +133,7 @@ export default function Home() {
               <div className="flex items-center justify-center rounded-xl border-2 border-dashed border-gray-200 bg-gray-50 py-32 text-gray-400">
                 {/*<Image src={`attractive.png`} alt={`Attractive png`} width={2514} height={1406}/>*/}
                 <ZoomableImage
-                  src="/attractive.png"
+                  src="attractive.png"
                   alt="Attractive png"
                   width={2514}
                   height={1406}
